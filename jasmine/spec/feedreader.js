a4e47c0a14a21f9e1cec2e61cc99b34328fd2cbb
hehongli <hehongli@migu.cn>
2018-07-26 17:56:42 +0800

    feature: new funcs
    11,12

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,6 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          * refe:https://blog.csdn.net/u011649436/article/details/10944489?locationNum=1
@@ -59,22 +58,44 @@ $(function() {
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
-        it("menu is hidden default",function () {
-
+        beforeEach(function () {
+            console.log($('body'));
+            console.log($('body').attr("class"));
         })
+
+        it("menu is hidden default",function () {
+            expect($('body').attr("class")).toContain("menu-hidden");
+        })
+
         /* TODO:
          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
          * 再次点击的时候是否隐藏。
          */
-        it("menu change statue when clicked",function () {
+        var testToggleFunc = function(){
+            it("menu toggle when clicked",function () {
+                if(times%2==1){
+                    expect($('body').attr("class")).not.toContain("menu-hidden");
+                }else {
+                    expect($('body').attr("class")).toContain("menu-hidden");
+                }
+            })
+        }
 
-        })
+        var menuIcon  = $('.menu-icon-link');
+        var times = 0;
+        menuIcon.on('click', function() {
+            times++;
+            testToggleFunc();
+        });
+        menuIcon.click();
+        menuIcon.click();
+
     })
 
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
-
+    describe("Initial Entries",function () {
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -82,11 +103,15 @@ $(function() {
          * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
          * 和异步的 done() 函数。
          */
+    })
+
 
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
-
+    describe("New Feed Selection",function () {
         /* TODO:
-         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-         * 记住，loadFeed() 函数是异步的。
-         */
+                 * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+                 * 记住，loadFeed() 函数是异步的。
+                 */
+    })
+
 }());
